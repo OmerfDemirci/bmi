26101fa1e387671bbb4d9b0072b8a7c1ed3e0b4a
Ömer Faruk Demirci <103588740+OmerfDemirci@users.noreply.github.com>
2022-11-23 14:21:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function calculate(){
+    var bmi;
+    var result = document.getElementById("result");
+
+    var weight = parseInt(document.getElementById("weight").
+    value);
+    document.getElementById("weight-val").textContent =
+    weight + " kg";
+
+    var height = parseInt(document.getElementById("height")
+    .value);
+    document.getElementById("height-val").textContent =
+    height + " cm";
+
+    bmi = (weight / Math.pow( (height/100), 2)).toFixed
+    (1);
+    result.textContent = bmi;
+
+    if(bmi < 18.5){
+        category = "Zayıf";
+        result.style.color = "#ffc44d";
+    }
+    else if( bmi >= 18.5 && bmi <= 24.9 ){
+        category = "Normal";
+        result.style.color = "#0be881";
+    }
+    else if( bmi >= 25 && bmi <= 29.9 ){
+        category = "Aşırı Kilolu";
+        result.style.color = "#ff884d";
+    }
+    else{
+        category = "Obezite";
+        result.style.color = "#ff5c57";
+    }
+    document.getElementById("category").textContent = category;
+}
